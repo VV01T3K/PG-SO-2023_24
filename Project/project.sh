22373a4e571fb7ced71906348982a9da283c8795
VV01T3K <wojteks.access@gmail.com>
2024-05-14 11:50:48 +0200

    works inteface not conversion

--- Project/project.sh
+++ Project/project.sh
@@ -105,90 +105,92 @@ getDetails() {
 }
 
 convert() {
-    # trap 'cleanup_and_exit' SIGINT
+    trap 'cleanup_and_exit' SIGINT
+    cleanup_and_exit() {
+        echo "Przerwanie konwersji..."
+        kill "$ffmpeg_pid" 2>/dev/null
+        rm -f ffmpeg_progress.log
+        rm -f "$temp_file"
+        exit 1
+    }
 
-    # cleanup_and_exit() {
-    #     echo "Przerwanie konwersji..."
-    #     kill "$ffmpeg_pid" 2>/dev/null
-    #     rm -f ffmpeg_progress.log
-    #     rm -f "$temp_file"
-    #     exit 1
-    # }
     local file=$1
-    echo "Converting $file"
-    # local format_options=""
-    # local first=true
-    # for format in "${supported_video_formats[@]}"; do
-    #     if $first; then
-    #         format_options+="TRUE $format "
-    #         first=false
-    #     else
-    #         format_options+="FALSE $format "
-    #     fi
-    # done
-    # # shellcheck disable=SC2086
-    # target_format=$(yad --title="Wybierz format" \
-    #     --no-selection \
-    #     --width=300 --height=300 \
-    #     --list --radiolist \
-    #     --print-column=2 --separator= \
-    #     --column=Select:BOOL \
-    #     --column=Format:TEXT $format_options)
+    local target_format
 
-    # if [ $? -eq 1 ]; then
-    #     echo "Cancel button was pressed."
-    #     return
-    # fi
-    # echo "Selected format: $target_format"
-    # local temp_file
-    # temp_file=$(mktemp --suffix=".${target_format}")
-    # local duration
-    # duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")
-    # duration=${duration%.*}
-    # ffmpeg -i "$file" "$temp_file" 2>ffmpeg_progress.log &
-    # ffmpeg_pid=$!
-    # local conversion_complete=0
-    # (
-    #     while kill -0 $ffmpeg_pid 2>/dev/null; do
-    #         current_time=$(grep -oP 'time=\K[\d:.]*' ffmpeg_progress.log | tail -1)
-    #         hours=$(echo "$current_time" | cut -d':' -f1)
-    #         minutes=$(echo "$current_time" | cut -d':' -f2)
-    #         seconds=$(echo "$current_time" | cut -d':' -f3)
-    #         current_seconds=$(echo "$hours*3600 + $minutes*60 + $seconds" | bc)
-    #         progress=$(echo "scale=2; $current_seconds/$duration*100" | bc)
-    #         echo "$progress"
-    #         sleep 1
-    #     done
-    #     echo "# Konwersja zakończona"
-    #     echo "100"
-    # ) | yad --progress --title="Postęp konwersji" --text="Trwa konwersja pliku..." --percentage=0 --auto-close && conversion_complete=1
+    local format_options=""
+    local first=true
+    for format in "${supported_video_formats[@]}"; do
+        if $first; then
+            format_options+="TRUE $format "
+            first=false
+        else
+            format_options+="FALSE $format "
+        fi
+    done
+    # shellcheck disable=SC2086
+    target_format=$(yad --title="Wybierz format" \
+        --no-selection \
+        --width=300 --height=300 \
+        --list --radiolist \
+        --print-column=2 --separator= \
+        --column=Select:BOOL \
+        --column=Format:TEXT $format_options)
 
-    # if [ $conversion_complete -ne 1 ]; then
-    #     echo "Konwersja nie została zakończona pomyślnie."
-    #     kill "$ffmpeg_pid" 2>/dev/null
-    #     rm -f "$temp_file"
-    #     yad --title="Błąd konwersji" --text="Konwersja nie została zakończona pomyślnie." --button=gtk-close:0
-    #     return
-    # fi
-    # local save_path
-    # save_path=$(yad --file --save --filename="${file%.*}.$target_format")
-    # if [ -z "$save_path" ]; then
-    #     yad --title="Błąd konwersji" --text="Konwersja nie została zakończona pomyślnie." --button=gtk-close:0
-    #     rm -f "$temp_file"
-    #     exit 1
-    # fi
+    if [ $? -eq 1 ]; then
+        echo "Cancel button was pressed."
+        return
+    fi
+    echo "Selected format: $target_format"
+    local temp_file
+    temp_file=$(mktemp --suffix=".${target_format}")
+    local duration
+    duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")
+    duration=${duration%.*}
+    ffmpeg -i "$file" "$temp_file" 2>ffmpeg_progress.log &
+    ffmpeg_pid=$!
+    local conversion_complete=0
+    (
+        while kill -0 $ffmpeg_pid 2>/dev/null; do
+            current_time=$(grep -oP 'time=\K[\d:.]*' ffmpeg_progress.log | tail -1)
+            hours=$(echo "$current_time" | cut -d':' -f1)
+            minutes=$(echo "$current_time" | cut -d':' -f2)
+            seconds=$(echo "$current_time" | cut -d':' -f3)
+            current_seconds=$(echo "$hours*3600 + $minutes*60 + $seconds" | bc)
+            progress=$(echo "scale=2; $current_seconds/$duration*100" | bc)
+            echo "$progress"
+            sleep 1
+        done
+        echo "# Konwersja zakończona"
+        echo "100"
+    ) | yad --progress --title="Postęp konwersji" --text="Trwa konwersja pliku..." --percentage=0 --auto-close && conversion_complete=1
 
-    # mv "$temp_file" "$save_path"
-    # if ! isInArray "$save_path" "${videos[@]}"; then
-    #     videos+=("$save_path")
-    # fi
-    # yad --title="Konwersja zakończona" --text="Plik został zapisany" --button=gtk-ok:0
-    # rm -f ffmpeg_progress.log
+    if [ $conversion_complete -ne 1 ]; then
+        echo "Konwersja nie została zakończona pomyślnie."
+        kill "$ffmpeg_pid" 2>/dev/null
+        rm -f "$temp_file"
+        yad --title="Błąd konwersji" --text="Konwersja nie została zakończona pomyślnie." --button=gtk-close:0
+        return
+    fi
+    local save_path
+    save_path=$(yad --file --save --filename="${file%.*}.$target_format")
+    if [ -z "$save_path" ]; then
+        yad --title="Błąd konwersji" --text="Konwersja nie została zakończona pomyślnie." --button=gtk-close:0
+        rm -f "$temp_file"
+        exit 1
+    fi
+
+    mv "$temp_file" "$save_path"
+    if ! isInArray "$save_path" "${videos[@]}"; then
+        videos+=("$save_path")
+    fi
+    yad --title="Konwersja zakończona" --text="Plik został zapisany" --button=gtk-ok:0
+    rm -f ffmpeg_progress.log
 }
 
 menu() {
-    args=()
-    index=0
+    local args=()
+    local index=0
+    local id
     for file in "${videos[@]}"; do
         filename=$(getDetails "$index" filename)
         format=$(getDetails "$index" format)
@@ -197,21 +199,24 @@ menu() {
         args+=("$index" "$filename" "$duration" "$format")
         index=$((index + 1))
     done
+
     id=$(yad --list \
         --title="Lista wczytanych plików" \
-        --width=700 --height=500 \
-        --column=ID:NUM \
-        --column=NAME:text \
-        --column=Duration:text \
-        --column=FORMAT:text \
-        --button=gtk-add:6 \
-        --button=gtk-delete:5 \
-        --button=gtk-remove:4 \
-        --button=Convert:3 \
+        --button=gtk-add:10 \
+        --button=gtk-remove:6 \
+        --button=gtk-delete:8 \
         --button=gtk-edit:2 \
-        --button=gtk-close:1 \
+        --button=Convert:4 \
         --button=gtk-media-play:0 \
-        --print-column=1 --separator= \
+        --button=gtk-close:1 \
+        --width=700 --height=500 \
+        --column=ID:num \
+        --column=NAME \
+        --column=Duration \
+        --column=FORMAT \
+        --print-column=1 \
+        --hide-column=1 \
+        --separator= \
         "${args[@]}")
 
     case $? in
@@ -226,22 +231,23 @@ menu() {
     2)
         echo "EDIT GO ON"
         ;;
-    3)
+    4)
+        echo "CONVERT"
         convert "${videos[$id]}"
         menu
         ;;
-    4)
+    6)
         echo "REMOVE"
         videos=("${videos[@]:0:$id}" "${videos[@]:$((id + 1))}")
         menu
         ;;
-    5)
+    8)
         echo "DELETE"
         rm -f "${videos[$id]}"
         videos=("${videos[@]:0:$id}" "${videos[@]:$((id + 1))}")
         menu
         ;;
-    6)
+    10)
         addNewFiles
         menu
         ;;
