1815dedd0b77069c3f346024bfdb82e43c74ecd8
VV01T3K <wojteks.access@gmail.com>
2024-05-16 19:51:07 +0200

    chore: Install celluloid if not already installed

--- Project/installDependencies.sh
+++ Project/installDependencies.sh
@@ -2,8 +2,14 @@
 
 LOGS="out.log"
 exec >$LOGS 2>&1
+if snap list | grep -qw "celluloid"; then
+    echo "celluloid is already installed."
+else
+    echo "Installing celluloid..."
+    sudo snap install celluloid
+fi
 
-required_packages=("yad" "ffmpeg" "totem" "gnome-icon-theme")
+required_packages=("yad" "ffmpeg" "gnome-icon-theme")
 echo "Updating package lists..."
 sudo apt-get update
 
